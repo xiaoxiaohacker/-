b21f10ec13acb028ab1e715636a41f9c39f484fc
xiaoxiaohacker <87226046+xiaoxiaohacker@users.noreply.github.com>
2021-11-29 22:53:32 +0800

    Update 5.c

--- C/数组的使用/实训/5.c
+++ C/数组的使用/实训/5.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
+//有一个a[3][4]整型二维数组，找出各行的最大值存到数组b[3]中
 void main()
 {
 	int a[3][4],i,j;
 	int b[3];
-	printf("������3�����и�ֵ:");
+	printf("请输入3行四列各值:");
 	for(i=0;i<3;i++)
 		for(j=0;j<4;j++)
 			scanf("%d",&a[i][j]);
-	printf("�������ֵΪ��\n");
+	printf("各行最大值为：\n");
 	for(i=0;i<3;i++)
 	{
-		b[i]=a[i][0];//ÿ����Ϊ���ֵ
+		b[i]=a[i][0];//每列设为最大值
 		for(j=0;j<4;j++)
 			if(b[i]<a[i][j])
 				b[i]=a[i][j];
